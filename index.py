9fba6c2cb22796fff8b19afe1f4b7bb8fea85d28
Roger Test <joybird2001@163.com>
2024-06-12 22:18:37 +0800

    complete loading a script file.
    add script file
    
    load script to memory.

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 from images.logo import logo
 from style.style import style_one
+from js import script
 
 def main():
     print('主函数执行！')
     logo()
     style_one()
+    script.run_script()
 
 
 if __name__ == "__main__":
---
+++ js/script.py
@@ -0,0 +1,2 @@
+def run_script():
+    print('>>>>>>>script running.')
