f1e18575fbd87532cf9934cd513ecf48f8abf5a6
Roger Test <joybird2001@163.com>
2024-06-14 06:14:21 +0800

    提交第一个主程序

--- index.py
+++ index.py
@@ -3,8 +3,9 @@ from style.style import style_one
 from js import script
 
 def main():
-    print('主函数执行！')
+    print('===>>> 主函数执行！')
     logo()
+    print('第一次修改')
     style_one()
     script.run_script()
     print('主程序执行完毕！')
